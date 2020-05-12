b4c8f4a14a7f9ebfd291b3151270d1d6c5133d45
Henrique <h.suel17@hotmail.com>
2020-05-11 21:38:10 -0300

    teste finalizado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useState , useEffect } from "react";
 
 import "./styles.css";
 
+import api from './services/api'
+
 function App() {
+  const [ repositories, setRepositories ] = useState([])
+  useEffect(() => {
+    api.get('/repositories').then(response => setRepositories([...response.data]))
+  },{})
+
   async function handleAddRepository() {
-    // TODO
+    const data = {
+      title: 'Henrique5',
+      url: 'Suel',
+      techs: ['vueJS']
+    }
+    api.post('/repositories',data).then(response => setRepositories([...repositories,response.data]))
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`repositories/${id}`).then(response => {
+      setRepositories(repositories.filter(r => r.id !== id))
+    })
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+          {repositories.map((value) => 
+          <li key={value.id}>
+            {value.title} 
+          <button onClick={() => handleRemoveRepository(value.id)}>
+          Remover
           </button>
-        </li>
+
+          </li>
+        )}
+
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
